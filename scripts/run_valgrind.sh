cf2511239079d4ad061de0a9b16ce9324aed2dd2
Bucovina <103101131+Bucovina@users.noreply.github.com>
2023-01-28 00:01:54 +0200

    V0.35

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -1,7 +1,7 @@
 cat "${INPUT_FILENAME}" | valgrind --leak-check=full --track-origins=yes --error-exitcode=0 ./bin/"${EXECUTABLE_NAME}" &
-sleep 10 && xdotool search --name "My Window" key X &&
-sleep 1 && xdotool search --name "My Window" key Y &&
-sleep 1 && xdotool search --name "My Window" key X &&
-sleep 1 && xdotool search --name "My Window" key Z &&
-sleep 2 && xdotool search --name "My Window" key Alt+F4
+sleep 10 && xdotool search --name "Clicker-Game" key X &&
+sleep 1 && xdotool search --name "Clicker-Game" key Y &&
+sleep 1 && xdotool search --name "Clicker-Game" key X &&
+sleep 1 && xdotool search --name "Clicker-Game" key Z &&
+sleep 2 && xdotool search --name "Clicker-Game" key Alt+F4
 
